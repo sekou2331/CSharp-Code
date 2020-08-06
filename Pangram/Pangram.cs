799e507867da9cc896653dd5a62514bc7bb2b30b
sekou2331 <sekou2331@gmail.com>
2020-08-05 22:33:25 -0400

    Pangram exercism
    This is the code I wrote for exercism

---
+++ Pangram/Pangram.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+public static class Pangram
+{
+	public static bool IsPangram(string input)
+		{
+		bool[] marked = new bool[26];
+
+		int index = 0;
+		string newIput = input.ToUpper();
+
+		for(int i = 0; i < newIput.Length; i++)
+			{
+				if ('A' <= newIput[i] && newIput[i] <= 'Z')
+				{
+
+					index = newIput[i] - 'A';
+
+				}else{
+				continue;
+
+				}
+				marked[index] = true; 
+
+			}
+		for (int i = 0; i <= 25; i++) 
+		if (marked[i] == false) 
+		return (false); 
+
+
+		return (true); 
+
+	}
+}
+  
