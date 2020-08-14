00b989baad3677879f8034c8b8203366bd30f5e4
sekou2331 <sekou2331@gmail.com>
2020-08-13 23:57:22 -0400

    Determine if a word or phrase is an isogram.
    An isogram (also known as a "nonpattern word") is a word or phrase without a repeating letter, however spaces and hyphens are allowed to appear multiple times.
    
    Examples of isograms:
    
    - lumberjacks
    - background
    - downstream
    - six-year-old
    
    The word *isograms*, however, is not an isogram, because the s repeats.

---
+++ isogram/Isogram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public static class Isogram
+{
+    public static bool IsIsogram(string word)
+    {
+        if (string.IsNullOrEmpty(word))
+            return true;
+        
+        
+        char previousLetter = word[0];
+        for (int i = 1; i < word.Length; i++)
+        {
+
+            if (word[i] == previousLetter)
+            {
+                return true;
+            }
+            else { previousLetter = word[i]; }
+        }
+  
+       return true;
+    }
+}
+//dotnet watch test
