8a592dc4be71bc85fc8a5b61b147d94e0af6807f
sekou2331 <sekou2331@gmail.com>
2020-08-16 12:06:09 -0400

    Convert a phrase to its acronym
    Help generate some jargon by writing a program that converts a long name
    like Portable Network Graphics to its acronym (PNG).

---
+++ acronym/Acronym.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public static class Acronym
+{
+    public static string Abbreviate(string phrase)
+    {
+
+            string capPhrase = phrase.ToUpper();
+            char[] arr = capPhrase.ToCharArray();
+            arr = Array.FindAll<char>(arr, (c => (char.IsLetterOrDigit(c)
+                                              || char.IsWhiteSpace(c)
+                                              )));
+            var str = new string(arr);
+
+
+            string[] splitPhrases = str.Split(' ');
+            string output = "";
+
+            foreach (string splitPhrase in splitPhrases)
+            {
+                if (String.IsNullOrEmpty(splitPhrase))
+                    continue;
+
+                output += splitPhrase[0];
+
+            }
+            Console.WriteLine(output);
+            return output;
+        }
+}
+
+//dotnet watch test
