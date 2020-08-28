c1a21fdbe92a1a1da41b8f04a4726a09d5af3b20
sekou2331 <sekou2331@gmail.com>
2020-08-28 02:44:19 -0400

    Leap Year script
    This determines if a year is a leap year

---
+++ leap/Leap.cs
@@ -0,0 +1,24 @@
+using System;
+
+public static class Leap
+{
+
+    public static bool IsLeapYear(int year) => ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) ? true : false;
+    /*
+    public static bool IsLeapYear(int year)
+    {
+        
+
+
+            if ((year % 4 == 0 &&  year % 100 != 0) || (year % 400 == 0))
+            {
+
+               return true;
+            }
+        
+            return false;
+
+        
+    }
+    */
+}
