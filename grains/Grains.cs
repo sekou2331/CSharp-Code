aa57e5d876fdbe2a624735e134f1f6664d39f95a
sekou2331 <sekou2331@gmail.com>
2020-09-04 14:25:22 -0400

    Grains code
    Used Geometric Sequences and Sums to figure this one out.
    Webpage:
    https://www.mathsisfun.com/algebra/sequences-sums-geometric.html

---
+++ grains/Grains.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public static class Grains
+{
+        public static ulong Square(int n)
+        {
+        if (n < 1 || n > 64)
+        throw new ArgumentOutOfRangeException("The number given is either a zero or a negative number and cant be used");
+
+
+        return (ulong)1 << (n - 1);
+
+        }
+
+        public static ulong Total()
+        {
+            ulong sum = 0;
+            for (var i = 1; i <= 64; i++)
+            {
+                sum += Square(i);
+            }
+            return sum;
+        }
+}
