a48164e34b6263c1f965d601b033c1b8e29e2365
sekou2331 <sekou2331@gmail.com>
2020-09-23 01:26:02 -0400

    Determine if a number is perfect, abundant, or deficient
    First try at this issue

---
+++ perfect-numbers/PerfectNumbers.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+public enum Classification
+{
+    Perfect,
+    Abundant,
+    Deficient
+}
+
+public static class PerfectNumbers
+{
+    public static Classification Classify(int number)
+    {
+            Classification perfect = Classification.Perfect;
+            Classification abundant = Classification.Abundant;
+            Classification deficient = Classification.Deficient;
+                
+            int sum = 0;
+            if(number <= 0)
+                 throw new ArgumentOutOfRangeException("The number given is either a zero or a negative number and cant be used");
+            for (int i = 0; i < number; i++)
+            {
+                if (i == 0)
+                {
+                    continue;
+                }
+                int numCheck = number % i;
+
+                if (numCheck == 0)
+                {
+                    sum += i;
+
+                }
+
+            }
+
+            
+
+            if (sum > number)
+            {
+                
+                Console.WriteLine("**Abundant**: aliquot sum > number");
+                return  abundant;
+            }
+            else if (sum < number)
+            {
+                
+                Console.WriteLine("**Deficient**: aliquot sum < number");
+               return deficient;
+            }
+                return perfect;
+           
+    }
+}
