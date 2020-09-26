adc87eafa2b419892cbf406480b74b9ba531acb8
sekou2331 <sekou2331@gmail.com>
2020-09-25 20:07:46 -0400

    Fixed

---
+++ gigasecond/Gigasecond.cs
@@ -0,0 +1,7 @@
+using System;
+
+public static class Gigasecond
+{
+    public static DateTime Add(DateTime moment)=> moment.AddSeconds((double)1000000000);
+    
+}
--- perfect-numbers/PerfectNumbers.cs
+++ perfect-numbers/PerfectNumbers.cs
@@ -1,54 +1,41 @@
 ﻿using System;
 
-public enum Classification
-{
+public enum Classification {
     Perfect,
     Abundant,
     Deficient
 }
 
-public static class PerfectNumbers
-{
-    public static Classification Classify(int number)
-    {
-            Classification perfect = Classification.Perfect;
-            Classification abundant = Classification.Abundant;
-            Classification deficient = Classification.Deficient;
-                
-            int sum = 0;
-            if(number <= 0)
-                 throw new ArgumentOutOfRangeException("The number given is either a zero or a negative number and cant be used");
-            for (int i = 0; i < number; i++)
-            {
-                if (i == 0)
-                {
-                    continue;
-                }
-                int numCheck = number % i;
+public static class PerfectNumbers {
+    public static Classification Classify (int number) {
 
-                if (numCheck == 0)
-                {
-                    sum += i;
+        if (number <= 0)
+            throw new ArgumentOutOfRangeException ();
 
-                }
+        int sum = 0;
+        for (int i = 0; i < number; i++) {
+            if (i == 0) {
+                continue;
+            }
+            int numCheck = number % i;
+
+            if (numCheck == 0) {
+                sum += i;
 
             }
+        }
 
-            
+        if (sum > number) {
 
-            if (sum > number)
-            {
-                
-                Console.WriteLine("**Abundant**: aliquot sum > number");
-                return  abundant;
-            }
-            else if (sum < number)
-            {
-                
-                Console.WriteLine("**Deficient**: aliquot sum < number");
-               return deficient;
-            }
-                return perfect;
-           
+            Console.WriteLine ("**Abundant**: aliquot sum > number");
+            return Classification.Abundant;
+        } else if (sum < number) {
+
+            Console.WriteLine ("**Deficient**: aliquot sum < number");
+            return Classification.Deficient;
+        }
+
+        return Classification.Perfect;;
+
     }
 }
